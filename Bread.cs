a966c3d0a1d4bf897c3e2c8074ae930deadb0110
Steven Ly <1stevenly92@gmail.com>
2020-05-08 11:33:25 -0700

    working backend for bread

--- Bread.cs
+++ Bread.cs
@@ -2,9 +2,23 @@ namespace Loaf
 {
     public class Bread
     {
-        public Bread()
+        private int _breadAmount;
+
+        private int _breadPrice;
+        public Bread(int breadAmount, int breadPrice)
         {
-            
+            _breadAmount = breadAmount;
+            _breadPrice = breadPrice;
+        }
+
+        public int GetBreadPrice()
+        {
+            return _breadPrice;
+        }
+
+        public void SetBread(int loafAmount)
+        {
+            _breadAmount = loafAmount;
         }
     }
 }
