e08b59ba4478a3673bb6b130f4f46594c4e2ec97
Steven Ly <1stevenly92@gmail.com>
2020-05-08 10:29:08 -0700

    got logic working for pasteries

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Bakery;
+using Poptarts;
 
 namespace BakeryShop
 {
@@ -8,15 +8,23 @@ namespace BakeryShop
     {
         public static void Main()
         {
+            Pastry pastryOrder  = new Pastry(2,0);
+
+            List<Pastry> Pastry = new List<Pastry>(){};
+            
+            //Console.WriteLine("Hello, Please press 1 for Bread and 2 for Pasteries. Please note there is a special. Bread: buy 2 get one free($5/per) or Pastery: 1 for $2 or 3 for $5");
+            Console.WriteLine("How many Pasteries");
+           string amount = Console.ReadLine();
+
+           int pasteries = int.Parse(amount);
+            pastryOrder.SetPastry(pasteries);
+            pastryOrder.SetPastryPrice(pasteries);
+            Console.WriteLine("Your Total is " + pastryOrder.GetPrice());
+
+
             
 
-            List<Pastry> newPastery = new List<Pastry>(){};
-            Console.WriteLine("Hello, Please press 1 for Bread and 2 for Pasteries. Please note there is a special. Bread: buy 2 get one free($5/per) or Pastery: 1 for $2 or 3 for $5");
-            newPastery.pastryAmount = Console.ReadLine();
-
-
-
-            Pastry newPastery = new Pastry(pastryAmount);
+            
         }
 
 
