84104a09191be406290483e2daf663da2eaed26f
Steven Ly <1stevenly92@gmail.com>
2020-05-08 08:56:58 -0700

    finished logic for pastery

---
+++ Bread.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using Poptarts;
+
+namespace Bakery
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("Hello, Please press 1 for Bread and 2 for Pasteries. Please note there is a special. Bread: buy 2 get one free($5/per) or Pastery: 1 for $2 or 3 for $5");
+        }
+    }
+}
